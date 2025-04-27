faec3ce04fad352f8f87e26d189e41fa5918aadc
Pathum Gawesh Gunasekara <147062281+PathumGunasekara@users.noreply.github.com>
2025-04-27 06:20:54 +0530

    update notification controller imports

--- backend/src/main/java/backend/Notification/controller/NotificationController.java
+++ backend/src/main/java/backend/Notification/controller/NotificationController.java
@@ -4,6 +4,11 @@ import backend.Notification.model.NotificationModel;
 import backend.Notification.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+
+
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
