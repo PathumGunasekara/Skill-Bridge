3ce349e5a51400901b46d4a4b7032fcd26e801ad
milni_methsani <milnimethshani123@gmail.com>
2025-05-16 01:44:25 +0530

    Update AllPost.js

--- frontend/src/Pages/PostManagement/AllPost.js
+++ frontend/src/Pages/PostManagement/AllPost.js
@@ -340,7 +340,7 @@ function AllPost() {
               <div className='empty-state-box modern'>
                 <div className='empty-icon'>
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="48" height="48">
-                    <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1. 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 3h-3v3h3V6zm0 4h-3v3h3v-3zm-4-4h-3v3h3V6zm0 4h-3v3h3v-3zm-4-4H6v3h3V6zm0 4H6v3h3v-3zm10 4H6v3h13v-3z"/>
+                    <path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-2 3h-3v3h3V6zm0 4h-3v3h3v-3zm-4-4h-3v3h3V6zm0 4h-3v3h3v-3zm-4-4H6v3h3V6zm0 4H6v3h3v-3zm10 4H6v3h13v-3z"/>
                   </svg>
                 </div>
                 <h2>No Posts Found</h2>
